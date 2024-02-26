6161863ac03718478c896dcb15ccaf83b9433291
Nikita Semenov <nsemenov.official@gmail.com>
2024-02-26 08:28:12 +0300

    Initial commit

---
+++ UnitTestFiles/SourceFiles/EcoLab1.c
@@ -0,0 +1,160 @@
+﻿/*
+ * <кодировка символов>
+ *   Cyrillic (UTF-8 with signature) - Codepage 65001
+ * </кодировка символов>
+ *
+ * <сводка>
+ *   EcoLab1
+ * </сводка>
+ *
+ * <описание>
+ *   Данный исходный файл является точкой входа
+ * </описание>
+ *
+ * <автор>
+ *   Copyright (c) 2018 Vladimir Bashev. All rights reserved.
+ * </автор>
+ *
+ */
+
+
+/* Eco OS */
+#include "IEcoSystem1.h"
+#include "IdEcoMemoryManager1.h"
+#include "IdEcoInterfaceBus1.h"
+#include "IdEcoFileSystemManagement1.h"
+#include "IdEcoLab1.h"
+
+/*
+ *
+ * <сводка>
+ *   Функция EcoMain
+ * </сводка>
+ *
+ * <описание>
+ *   Функция EcoMain - точка входа
+ * </описание>
+ *
+ */
+int16_t EcoMain(IEcoUnknown* pIUnk) {
+	int16_t index = 0;
+    int16_t result = -1;
+    /* Указатель на системный интерфейс */
+    IEcoSystem1* pISys = 0;
+    /* Указатель на интерфейс работы с системной интерфейсной шиной */
+    IEcoInterfaceBus1* pIBus = 0;
+    /* Указатель на интерфейс работы с памятью */
+    IEcoMemoryAllocator1* pIMem = 0;
+    int16_t* arr = 0;
+    /* Указатель на тестируемый интерфейс */
+    IEcoLab1* pIEcoLab1 = 0;
+
+    /* Проверка и создание системного интрефейса */
+    if (pISys == 0) {
+        result = pIUnk->pVTbl->QueryInterface(pIUnk, &GID_IEcoSystem1, (void **)&pISys);
+        if (result != 0 && pISys == 0) {
+        /* Освобождение системного интерфейса в случае ошибки */
+            goto Release;
+        }
+    }
+
+    /* Получение интерфейса для работы с интерфейсной шиной */
+    result = pISys->pVTbl->QueryInterface(pISys, &IID_IEcoInterfaceBus1, (void **)&pIBus);
+    if (result != 0 || pIBus == 0) {
+        /* Освобождение в случае ошибки */
+        goto Release;
+    }
+#ifdef ECO_LIB
+    /* Регистрация статического компонента для работы со списком */
+    result = pIBus->pVTbl->RegisterComponent(pIBus, &CID_EcoLab1, (IEcoUnknown*)GetIEcoComponentFactoryPtr_1F5DF16EE1BF43B999A434ED38FE8F3A);
+    if (result != 0 ) {
+        /* Освобождение в случае ошибки */
+        goto Release;
+    }
+#endif
+    /* Получение интерфейса управления памятью */
+    result = pIBus->pVTbl->QueryComponent(pIBus, &CID_EcoMemoryManager1, 0, &IID_IEcoMemoryAllocator1, (void**) &pIMem);
+
+    /* Проверка */
+    if (result != 0 || pIMem == 0) {
+        /* Освобождение системного интерфейса в случае ошибки */
+        goto Release;
+    }
+
+    /* Выделение блока памяти */
+	arr = (int16_t*)pIMem->pVTbl->Alloc(pIMem, 10);
+
+    /* Заполнение блока памяти */
+    pIMem->pVTbl->Fill(pIMem, arr, 0, 10 * sizeof(int16_t));
+	
+	for (index; index < 10; index++) {
+		printf("%d ", arr[index]);
+	}
+
+	printf("\n");
+
+	arr[0] = -2;
+    arr[1] = 7;
+    arr[2] = 15;
+    arr[3] = -14;
+    arr[4] = 0;
+    arr[5] = 15;
+    arr[6] = 0;
+    arr[7] = 7;
+    arr[8] = -7;
+    arr[9] = -4;
+
+	for (index = 0; index < 10; index++) {
+		printf("%d ", arr[index]);
+	}
+
+	printf("\n");
+
+
+
+    /* Получение тестируемого интерфейса */
+    result = pIBus->pVTbl->QueryComponent(pIBus, &CID_EcoLab1, 0, &IID_IEcoLab1, (void**) &pIEcoLab1);
+    if (result != 0 || pIEcoLab1 == 0) {
+        /* Освобождение интерфейсов в случае ошибки */
+        goto Release;
+    }
+
+	result = pIEcoLab1->pVTbl->qsort(pIEcoLab1, arr, 10, 2);
+
+    //result = pIEcoLab1->pVTbl->MyFunction(pIEcoLab1, name, &copyName);
+
+	for (index = 0; index < 10; index++) {
+		printf("%d ", arr[index]);
+	}
+
+	printf("\n");
+
+
+    /* Освлбождение блока памяти */
+    pIMem->pVTbl->Free(pIMem, arr);
+
+Release:
+
+    /* Освобождение интерфейса для работы с интерфейсной шиной */
+    if (pIBus != 0) {
+        pIBus->pVTbl->Release(pIBus);
+    }
+
+    /* Освобождение интерфейса работы с памятью */
+    if (pIMem != 0) {
+        pIMem->pVTbl->Release(pIMem);
+    }
+
+    /* Освобождение тестируемого интерфейса */
+    if (pIEcoLab1 != 0) {
+        pIEcoLab1->pVTbl->Release(pIEcoLab1);
+    }
+
+
+    /* Освобождение системного интерфейса */
+    if (pISys != 0) {
+        pISys->pVTbl->Release(pISys);
+    }
+
+    return result;
+}
